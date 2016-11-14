07f57fff3a18b3adfc8a3e7a8428487665428659
Robin Michay <robin.michay@gmail.com>
2016-11-14 22:12:57 +0100

    add babel and babel watch

--- gulpfile.js
+++ gulpfile.js
@@ -5,13 +5,22 @@
     wrap = require('gulp-wrap'),
     declare = require('gulp-declare'),
     concat = require('gulp-concat'),
-    merge = require('merge-stream');
+    merge = require('merge-stream'),
+    babel = require('gulp-babel');
 
 gulp.task('scss', function() {
   return gulp.src('src/styles/scss/style.scss')
     .pipe(sass())
     .pipe(gulp.dest('dist/styles'))
     .pipe(sync.stream());
+});
+
+gulp.task('babelify', function() {
+  gulp.src('src/js/app/main.js')
+    .pipe(babel({
+      presets: ['es2015']
+    }))
+    .pipe(gulp.dest('dist/scripts'));
 });
 
 gulp.task('compile-templates', function() {
@@ -42,12 +51,13 @@ gulp.task('compile-templates', function() {
     .pipe(gulp.dest('dist/scripts'));
 });
 
-gulp.task('sync', ['scss', 'compile-templates'], function() {
+gulp.task('sync', ['scss', 'compile-templates', 'babelify'], function() {
   sync.init({
     server: './'
   });
   gulp.watch("src/styles/scss/**/*.scss", ['scss']);
   gulp.watch('src/templates/**/*.hbs', ['compile-templates']);
+  gulp.watch('src/js/app/main.js', ['babelify']);
 });
 
 gulp.task('default', ['sync'], function() {
