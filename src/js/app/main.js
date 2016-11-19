cdd6604d868599f4091e606d95ec1c0f302d8ec4
clemishow <clement.saulnier@hetic.net>
2016-11-19 14:48:12 +0100

    [LOGIN] styles done for mobile | [SASS] re-structure scsss files with pages, components.. | Added reset.min.css | Disable zoom on mobile devices | [IMAGES] re-structure folder with a icons folder

--- src/js/app/main.js
+++ src/js/app/main.js
@@ -1,3 +1,6 @@
+// IMPORT ROUTING FROM PROVIDERS
 import {Routing} from './providers';
+
+// NEW ROUTING
 let routing = new Routing();
 routing.goToPage('login', {title: 'Connexion'});
--- src/js/app/providers.js
+++ src/js/app/providers.js
@@ -1,14 +1,29 @@
 let templates = window.MyApp.templates;
 let render = document.getElementsByTagName('render-outlet')[0];
 
+// ROUTING CLASS
 export class Routing {
   constructor() {
     this.render = render;
   }
+
+  /**
+  * Function goToPage()
+  * Go to a page including the template
+  * @param page (the template/page)
+  * @param infos (infos to push into the template like the title of the page)
+  */
+
   goToPage(page, infos) {
     this.render.innerHTML = templates[page](infos);
     this.removeLinks();
   }
+
+  /**
+  * Function removeLinks()
+  * All link <a href="/"> are remove and replace by the function goToPage()
+  */
+
   removeLinks() {
     let self = this;
     let links = this.render.getElementsByTagName('a');
