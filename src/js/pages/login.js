733844ae24fec45e8f47b839c2399268ebd9a89b
clemishow <clement.saulnier@hetic.net>
2016-11-23 21:28:00 +0100

    Notification error and success

--- src/js/pages/login.js
+++ src/js/pages/login.js
@@ -17,13 +17,34 @@ export class LoginPage {
 
   // LOOP INTO USERS AND CHECK IF PASSWORD AND EMAIL MATCH AND THEN PUSH HOMEPAGE
   login() {
+    // INPUTS
     let email = document.querySelector('input#email').value;
     let password = document.querySelector('input#password').value;
+    // NOTIFICATIONS
+    let notification = document.querySelector('.notifications');
+    let notificationMessage = document.querySelector('.notifications__message');
+    let notificationIcon = document.querySelector('.notifications__icon img');
+
     self.log = new LogService();
     let user = self.log.login(email, password);
     if (user) {
+
+      // NOTIFICATION SUCCESS
+      notification.classList.add('success');
+      notificationMessage.innerHTML = 'Votre connexion a été un succès !';
+      notification.style.transform = 'translate(0px)';
+      notificationIcon.src = 'src/images/icons/success-icon.svg';
+
+
       this.cache.setCache('user', user);
       this.rout.navToRoot('home', {rout: this.rout, cache: this.cache});
+    } else {
+
+      // NOTIFICATION ERROR
+      notification.classList.add('danger');
+      notificationMessage.innerHTML = 'Votre connexion a échouée, veuillez réessayer';
+      notification.style.transform = 'translate(0px)';
+      notificationIcon.src = 'src/images/icons/danger-icon.svg';
     }
   }
 }
